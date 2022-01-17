d44d49cbdc1cec2ada68bb2aa7b2b40a4dab3700
Valeria Kozlova <v.kozlova@aventica.ru>
2022-01-17 18:02:04 +0100

    add variable, pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script></script>
+
+<div class="about">
+  <h1>About us</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius officia
+    voluptates sed labore, deserunt accusamus. Labore, esse cum accusantium
+    alias eveniet recusandae laudantium, et beatae vitae unde odio, error sit.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script>
+  let title = 'Welcome'
+
+  const updateTitle = () => {
+    title = 'New title'
+  }
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum, a minus
+    excepturi, laborum ducimus numquam autem modi provident vel officia magnam
+    ea nemo dolore sit, soluta veritatis non quaerat voluptatem.
+  </p>
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
